d8be9fa5b9abbfb1cbc5103f4bf0f6c1b10d3d99
Unknown <bariseray@gmail.com>
2016-10-29 13:11:13 +0100

    Add TrackApi
    Merged from personal repo to group repo.

---
+++ TrackApi/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/{paramX}&{paramY}', function ($paramX, $paramY) use ($app) {
+
+    $results = DB::select("SELECT *, ( 3959 * acos( cos( radians($paramX) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians($paramY) ) + sin( radians($paramX) ) * sin( radians( latitude ) ) ) ) AS distance FROM properties HAVING distance < 80 ORDER BY distance LIMIT 0 , 200");
+
+    return $results;
+
+});
