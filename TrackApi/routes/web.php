905cf043a47193c91247cae6801b200dee7f514e
Baris Eray <bariseray@gmail.com>
2016-11-07 20:44:14 +0000

    Testing Grid v1

--- TrackApi/routes/web.php
+++ TrackApi/routes/web.php
@@ -22,7 +22,35 @@ $app->get('/clean/{paramLatitude}&{paramLongitude}/{paramYear}&{paramRadious}&{p
     WHERE yearSold = ($paramYear) HAVING distance < ($paramRadious)
     ORDER BY distance LIMIT 0, $paramLimit");
 
-    return $results;
+    //return $results;
+
+      $mapGrid = false;
+      $mapData = false;
+      $radius = 0.001; //grid size is multiplied by 2.8 to convert from the heat map radius to lat/long values(works for my lat/long, maybe not yours). * 0.3 is arbitrary to avoid seeing the grid on the map.
+      $string = json_encode($results);
+      $json_a = json_decode($string, true);
+
+      forEach($json_a as $key => $value){
+          $row = intval(round(($value['latitude'] / $radius)));
+          $column = intval(round(($value['longitude'] / $radius)/68*111)); //around 52.0;5.0 latitude needs to be scaled to make a square grid with the used longitude grid size
+          if(isset($mapGrid[$row][$column])){
+              $mapGrid[$row][$column] = round(($value['avgYearPostcodeNorm'] + $mapGrid[$row][$column]) / 2);
+          } else {
+              $mapGrid[$row][$column] = $value['avgYearPostcodeNorm'];
+          }
+      }
+
+      forEach($mapGrid as $long => $array){
+          forEach($array as $lat => $weight){
+              $mapData[] = array(
+                  "latitude" => $long * $radius,
+                  "longitude" => ($lat * $radius)/111*68,
+                  "avgYearPostcodeNorm" => $weight
+              );
+          }
+      }
+
+      return $mapData;
 
 });
 
@@ -92,6 +120,7 @@ $app->get('/dirtyDebug/{paramLatitude}&{paramLongitude}/{paramYear}&{paramRadiou
 
 });
 
+//TODO: Remove for production, used for the default example test.
 $app->get('/', function () use ($app) {
     return $app->version();
 });
