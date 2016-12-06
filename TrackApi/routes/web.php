ae3a29483b13514a15005edb2753f155af88eaa6
Baris Eray <bariseray@gmail.com>
2016-12-06 16:54:46 +0000

    Third Attempt Grid Zoom

--- TrackApi/routes/web.php
+++ TrackApi/routes/web.php
@@ -36,7 +36,7 @@ $app->get('/{androidId}/clean/{paramLatitude}&{paramLongitude}/{paramYear}&{para
     //Average values that fall within a grid cell
     $mapGrid = false;
     $mapData = false;
-    $radius = 0.00001 * $paramZoom;
+    $radius = 0.0001 * $paramZoom;
     $string = json_encode($results);
     $json_a = json_decode($string, true);
 
